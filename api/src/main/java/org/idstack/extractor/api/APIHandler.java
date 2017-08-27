1b61b8365c7edd587c563b07568c6a38b4269582
Chanaka Lakmal <ldclakmal@gmail.com>
2017-08-27 18:39:03 +0530

    Add api module classes

---
+++ api/src/main/java/org/idstack/extractor/api/APIHandler.java
@@ -0,0 +1,82 @@
+package org.idstack.extractor.api;
+
+import org.idstack.extractor.feature.Constant;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * @author Chanaka Lakmal
+ * @date 24/4/2017
+ * @since 1.0
+ */
+
+@RestController
+public class APIHandler {
+
+    Router router = new Router();
+
+    @RequestMapping("/")
+    public void root(HttpServletResponse httpServletResponse) throws IOException {
+        httpServletResponse.sendRedirect("http://idstack.one/validator");
+    }
+
+    @RequestMapping(value = "/{version}/validator/sign", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public String signDocument(@PathVariable("version") String version, @RequestBody String json) {
+        return router.signDocument(json);
+    }
+
+    @RequestMapping(value = "/{version}/saveconfig/basic", method = RequestMethod.POST)
+    @ResponseBody
+    public boolean saveBasicConfiguration(@PathVariable("version") String version, @RequestHeader("Organization") String org, @RequestHeader("Email") String email) {
+        return router.saveBasicConfiguration(org, email);
+    }
+
+    @RequestMapping(value = "/{version}/saveconfig/document", method = RequestMethod.POST)
+    @ResponseBody
+    public boolean saveDocumentConfiguration(@PathVariable("version") String version, @RequestHeader("Document") Map<String, String> configurations) {
+        return router.saveDocumentConfiguration(configurations);
+    }
+
+    @RequestMapping(value = "/{version}/saveconfig/whitelist", method = RequestMethod.POST)
+    @ResponseBody
+    public boolean saveWhiteListConfiguration(@PathVariable("version") String version, @RequestHeader("WhiteList") Map<String, String> configurations) {
+        return router.saveWhiteListConfiguration(configurations);
+    }
+
+    @RequestMapping(value = "/{version}/saveconfig/blacklist", method = RequestMethod.POST)
+    @ResponseBody
+    public boolean saveBlackListConfiguration(@PathVariable("version") String version, @RequestHeader("BlackList") Map<String, String> configurations) {
+        return router.saveBlackListConfiguration(configurations);
+    }
+
+    @RequestMapping(value = "/{version}/getconfig/{type}/{property}", method = RequestMethod.GET)
+    @ResponseBody
+    public Object getConfigurationFile(@PathVariable("version") String version, @PathVariable("type") String type, @PathVariable("property") String property) {
+        return router.getConfiguration(type, property);
+    }
+
+    @RequestMapping(value = "/{version}/savepubcert", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @ResponseBody
+    public boolean savePublicCertificate(@PathVariable("version") String version, @RequestParam(value = "cert") final MultipartFile certificate) {
+        return router.saveCertificate(Constant.GlobalAttribute.PUB_CERTIFICATE, certificate, null);
+    }
+
+    @RequestMapping(value = "/{version}/getpubcert/{uuid}", method = RequestMethod.GET, produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    @ResponseBody
+    public FileSystemResource getPublicCertificate(@PathVariable("version") String version, @PathVariable("uuid") String uuid) {
+        return router.getPublicCertificate(uuid);
+    }
+
+    @RequestMapping(value = "/{version}/savepvtcert", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @ResponseBody
+    public boolean savePrivateCertificate(@PathVariable("version") String version, @RequestParam(value = "cert") final MultipartFile certificate, @RequestHeader("Password") String password) {
+        return router.saveCertificate(Constant.GlobalAttribute.PVT_CERTIFICATE, certificate, password);
+    }
+}
---
+++ api/src/main/java/org/idstack/extractor/api/Router.java
@@ -0,0 +1,74 @@
+package org.idstack.extractor.api;
+
+import org.idstack.extractor.feature.Constant;
+import org.idstack.extractor.feature.FeatureImpl;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * @author Chanaka Lakmal
+ * @date 23/8/2017
+ * @since 1.0
+ */
+public class Router {
+
+    public boolean saveBasicConfiguration(String org, String email) {
+        return FeatureImpl.getFactory().saveBasicConfiguration(org, email);
+    }
+
+    public boolean saveDocumentConfiguration(Map<String, String> configurations) {
+        return FeatureImpl.getFactory().saveDocumentConfiguration(configurations);
+    }
+
+    public boolean saveWhiteListConfiguration(Map<String, String> configurations) {
+        return FeatureImpl.getFactory().saveWhiteListConfiguration(configurations);
+    }
+
+    public boolean saveBlackListConfiguration(Map<String, String> configurations) {
+        return FeatureImpl.getFactory().saveBlackListConfiguration(configurations);
+    }
+
+    public Object getConfiguration(String type, String property) {
+        String src = null;
+        switch (type) {
+            case "basic":
+                src = Constant.GlobalAttribute.BASIC_CONFIG_FILE_NAME;
+                break;
+            case "document":
+                src = Constant.GlobalAttribute.DOCUMENT_CONFIG_FILE_NAME;
+                break;
+            case "whitelist":
+                src = Constant.GlobalAttribute.WHITELIST_CONFIG_FILE_NAME;
+                break;
+            case "blacklist":
+                src = Constant.GlobalAttribute.BLACKLIST_CONFIG_FILE_NAME;
+                break;
+            default:
+                break;
+        }
+        return FeatureImpl.getFactory().getConfiguration(src, property);
+    }
+
+    public boolean saveCertificate(String category, MultipartFile certificate, String password) {
+        File file = FeatureImpl.getFactory().saveCertificate(category, password);
+        try {
+            certificate.transferTo(file);
+            return true;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public FileSystemResource getPublicCertificate(String uuid) {
+        return new FileSystemResource(new File(FeatureImpl.getFactory().getPublicCertificate(uuid)));
+    }
+
+
+    public String signDocument(String json) {
+        return FeatureImpl.getFactory().signDocument(json);
+    }
+}
