79fee11b4240d4d76669583e32800e732fc44f4b
ldclakmal <ldclakmal@gmail.com>
2017-09-03 19:09:16 +0530

    Implement creating MR functionality

--- api/src/main/java/org/idstack/extractor/api/Router.java
+++ api/src/main/java/org/idstack/extractor/api/Router.java
@@ -1,12 +1,21 @@
 package org.idstack.extractor.api;
 
+import org.bouncycastle.cms.CMSException;
+import org.bouncycastle.operator.OperatorCreationException;
 import org.idstack.extractor.JsonCreator;
+import org.idstack.extractor.JsonExtractor;
 import org.idstack.feature.Constant;
 import org.idstack.feature.FeatureImpl;
 import org.springframework.stereotype.Component;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
 
 /**
  * @author Chanaka Lakmal
@@ -25,7 +34,15 @@ public class Router {
     public final String pubCertType = FeatureImpl.getFactory().getProperty(getPropertiesFile(), Constant.GlobalAttribute.PUB_CERTIFICATE_TYPE);
 
     public String createMR(String json) {
-        return new JsonCreator().constructAsNestedJson(json);
+        String formattedJson = new JsonCreator().constructAsNestedJson(json);
+        JsonExtractor jsonExtractor = new JsonExtractor(FeatureImpl.getFactory().getPrivateCertificateFilePath(configFilePath, pvtCertFilePath, pvtCertType),
+                FeatureImpl.getFactory().getPassword(configFilePath, pvtCertFilePath, pvtCertPasswordType),
+                FeatureImpl.getFactory().getPublicCertificateURL(configFilePath, pubCertFilePath, pubCertType));
+        try {
+            return jsonExtractor.signExtactedJson(formattedJson);
+        } catch (CertificateException | UnrecoverableKeyException | NoSuchAlgorithmException | KeyStoreException | CMSException | OperatorCreationException | NoSuchProviderException | IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private FileInputStream getPropertiesFile() {
