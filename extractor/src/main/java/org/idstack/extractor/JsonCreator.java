e11031976f622ce558b9f99b6d78ccc9ea9626b2
ldclakmal <ldclakmal@gmail.com>
2017-09-03 19:08:21 +0530

    Improve json creating

--- extractor/src/main/java/org/idstack/extractor/JsonCreator.java
+++ extractor/src/main/java/org/idstack/extractor/JsonCreator.java
@@ -4,6 +4,8 @@ import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import org.idstack.feature.Constant;
+import org.idstack.feature.document.*;
 
 import java.util.*;
 
@@ -17,12 +19,29 @@ public class JsonCreator {
 
     public String constructAsNestedJson(String receivedJson) {
         JsonObject obj = new JsonParser().parse(receivedJson).getAsJsonObject();
-        LinkedHashMap<String, Object> finalVersion = constructJson(obj);
-        String jsonStringToSign = new Gson().toJson(finalVersion);
+        JsonObject metadataObject = obj.getAsJsonObject(Constant.JsonAttribute.META_DATA);
+        JsonObject contentObject = obj.getAsJsonObject(Constant.JsonAttribute.CONTENT);
+
+        //create metadata object
+        MetaData metaData = new MetaData(metadataObject.get(Constant.JsonAttribute.MetaData.NAME).getAsString(), metadataObject.get(Constant.JsonAttribute.MetaData.VERSION).getAsString(), metadataObject.get(Constant.JsonAttribute.MetaData.DOCUMENT_ID).getAsString(), metadataObject.get(Constant.JsonAttribute.MetaData.DOCUMENT_TYPE).getAsString(), new Gson().fromJson(metadataObject.get(Constant.JsonAttribute.MetaData.ISSUER).toString(), Issuer.class));
+
+        //create linked hash map
+        LinkedHashMap<String, Object> contentMap = constructJsonContent(contentObject);
+
+        //create empty extractor
+        Extractor extractor = new Extractor("", new Signature("", ""));
+
+        //create empty validator array
+        ArrayList<Validator> validators = new ArrayList<Validator>();
+
+        Document doc = new Document(metaData, contentMap, extractor, validators);
+
+        String jsonStringToSign = new Gson().toJson(doc);
+
         return jsonStringToSign;
     }
 
-    private LinkedHashMap<String, Object> constructJson(JsonObject obj) {
+    private LinkedHashMap<String, Object> constructJsonContent(JsonObject obj) {
         Set<Map.Entry<String, JsonElement>> set = obj.entrySet();
         Iterator<Map.Entry<String, JsonElement>> iterator = set.iterator();
         LinkedHashMap<String, Object> map = new LinkedHashMap<>();
