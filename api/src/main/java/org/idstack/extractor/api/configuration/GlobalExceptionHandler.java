729a224b7175afea78e8b636c586867909a39413
ldclakmal <ldclakmal@gmail.com>
2017-10-13 18:28:52 +0530

    Update exception handler

--- api/src/main/java/org/idstack/extractor/api/configuration/GlobalExceptionHandler.java
+++ api/src/main/java/org/idstack/extractor/api/configuration/GlobalExceptionHandler.java
@@ -1,9 +1,13 @@
 package org.idstack.extractor.api.configuration;
 
+import com.google.gson.Gson;
+import org.idstack.feature.Constant;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Collections;
 
 /**
  * @author Chanaka Lakmal
@@ -15,7 +19,7 @@ public class GlobalExceptionHandler {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(RuntimeException.class)
-    public String  handle(RuntimeException e) {
-        return e.getMessage();
+    public String handle(RuntimeException e) {
+        return new Gson().toJson(Collections.singletonMap(Constant.Status.STATUS, e.getMessage()));
     }
 }
